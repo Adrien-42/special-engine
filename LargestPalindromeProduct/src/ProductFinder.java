68105c40875cb8b6f339f7c5e1b5418a4af671d8
Adrien <adriennev.42@gmail.com>
2016-11-08 10:51:01 -0500

    Added Current LargestPalindromeProduct Code for Project Euler

--- LargestPalindromeProduct/src/ProductFinder.java
+++ LargestPalindromeProduct/src/ProductFinder.java
@@ -1,36 +1,53 @@
+import java.util.concurrent.TimeUnit;
+
 public class ProductFinder {
-    public int multiplier(int num1, int num2){
-        return num1*num2;
-    }
     public boolean palindromeChecker(int number){
         String potentialPalindrome = Integer.toString(number);
         String subString1;
         String subString2;
+        String subString3;
 
         if(potentialPalindrome.length()%2==0){
             subString1 = potentialPalindrome.substring(0, (potentialPalindrome.length()/2));
-            System.out.println(subString1);
+            subString3 = potentialPalindrome.substring((potentialPalindrome.length())/2, potentialPalindrome.length());
             subString2 = new StringBuffer(subString1).reverse().toString();
-            System.out.println(subString2);
 
-            //Switch to contain substring?
-            if(subString2 == potentialPalindrome.substring(potentialPalindrome.length()/2, potentialPalindrome.length())){
+            if(subString2==subString3){
                 return true;
             }
             return false;
         } else{
-            subString1 = potentialPalindrome.substring(0, (potentialPalindrome.length()/2)-2);
+            subString1 = potentialPalindrome.substring(0, ((potentialPalindrome.length()-1)/2));
+            subString3 = potentialPalindrome.substring((potentialPalindrome.length()+1)/2, potentialPalindrome.length());
             subString2 = new StringBuffer(subString1).reverse().toString();
 
-            if(subString2 == potentialPalindrome.substring((potentialPalindrome.length()/2)+1, potentialPalindrome.length()-1)){
+            if(subString2==subString3){
                 return true;
             }
             return false;
         }
     }
+    //???
+    public int greatestPalindrome() {
+        int greatest = 0;
+        for(int i = 100; i <=999; i++){
+            for(int j = 100; j <=999; j++){
+                if(palindromeChecker(i*j) && i*j > greatest){
+                    greatest = i*j;
+                    System.out.println(greatest);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return greatest;
+    }
     public static void main(String[] args){
         ProductFinder finder = new ProductFinder();
 
-        System.out.println(finder.palindromeChecker(9009));
+        System.out.println(finder.greatestPalindrome());
     }
 }
