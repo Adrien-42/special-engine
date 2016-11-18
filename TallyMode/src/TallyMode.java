7ec7c7e48b6a754a927abf014bfaf3e997cdbe86
Adrien <adriennev.42@gmail.com>
2016-11-18 09:48:51 -0500

    Added Command Line Paramiters Test

--- TallyMode/src/TallyMode.java
+++ TallyMode/src/TallyMode.java
@@ -6,7 +6,7 @@ public class TallyMode {
 
         for(int i=0; i < arr.length; i++){
             if (highest[0] < arr[i]){
-                highest[0] = arr[i];
+                highest[0] = arr[i]; //Switch to index
             }else if (highest[0] == arr[i]){
                 highest[placeInArray] = arr[i];
                 placeInArray++;
