715885b8efabbe77d479f71cfe7e35ac7af8c905
Adrien <adriennev.42@gmail.com>
2017-03-09 11:48:46 -0500

    Added untested solution for finding side

--- Pyth/src/class1.java
+++ Pyth/src/class1.java
@@ -1,3 +1,25 @@
 public class class1 {
+    //Returns side if valid, otherwise returns zero
+    public int triangleChecker(int x, int y){
+        int bigger;
+        int smaller;
 
+        if(x>y){
+            bigger=x;
+            smaller=y;
+        }else{
+            bigger=y;
+            smaller=x;
+        }
+
+        double side1 = Math.sqrt((bigger*bigger)+(smaller*smaller));
+        double side2 = Math.sqrt((bigger*bigger)-(smaller*smaller));
+
+        if(side1/((int)side1)==1.0){
+            return (int)side1;
+        }else if(side2/((int)side2)==1.0){
+            return (int)side2;
+        }
+        return 0;
+    }
 }
